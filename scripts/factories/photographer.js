c92c58400a3a5bede90009bf8d6be1ffc93534e0
Gael Foini <gaelfoini@gmail.com>
2022-11-25 18:47:27 +0100

    index build, display data and link works

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,38 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+  // get data from the imported object in fetch of json
+  const { name, portrait, city, country, tagline, price, id } = data;
+  // define path for picture
+  const picture = `assets/photographers/${portrait}`;
 
-    const picture = `assets/photographers/${portrait}`;
+  // create user card in DOM
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    article.setAttribute("aria-label", "carte photographe");
+    const link = document.createElement("a");
+    const pathId = `./photographer.html?=${id}`;
+    link.setAttribute("href", pathId);
+    link.setAttribute("aria-label", "lien vers artiste");
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    img.setAttribute("alt", "portrait photographe");
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    const h3 = document.createElement("h3");
+    h3.textContent = city + ", " + country;
+    const divTag = document.createElement("div");
+    divTag.setAttribute("id", "tagline");
+    divTag.textContent = tagline;
+    const divPrice = document.createElement("div");
+    divPrice.setAttribute("id", "price");
+    divPrice.textContent = price + "€/jour";
+    article.appendChild(link);
+    link.appendChild(img);
+    link.appendChild(h2);
+    article.appendChild(h3);
+    article.appendChild(divTag);
+    article.appendChild(divPrice);
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+    return article;
+  }
+  return { name, picture, city, country, tagline, price, getUserCardDOM };
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,32 @@
-//Mettre le code JavaScript lié à la page photographer.html
+//Mettre le code JavaScript lié à la page photographer.html// import data from json
+async function getPhotographers() {
+    return fetch("./data/photographers.json")
+      .then(function (res) {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  
+  async function displayData(photographers) {
+    const headerSection = document.querySelector(".photograph-header")
+    photographers.forEach((photographer) => {
+      const photographerModel = headerFactory(photographer);
+      const userCardDOM = photographerModel.getUserCardDOM();
+      headerSection.appendChild(userCardDOM);
+    });
+  }
+  
+  
+  
+  async function init() {
+    // Récupère les datas des photographes
+    const { photographers } = await getPhotographers();
+    displayData(photographers);
+  }
+  
+  init();
+  
